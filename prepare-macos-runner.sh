d711f60d4384491ae559c3d68dae85b56066c67e
Paul Zabelin <paulz@users.noreply.github.com>
2020-08-30 00:57:05 -0700

    add sudo to kill

--- prepare-macos-runner.sh
+++ prepare-macos-runner.sh
@@ -9,12 +9,12 @@ sudo scutil --set LocalHostName "$UniqueComputerName"
 sudo scutil --set ComputerName "$UniqueComputerName"
 
 # Close Notification window
-killall UserNotificationCenter || true
+sudo killall UserNotificationCenter || true
 
 # Do not disturb
 defaults -currentHost write com.apple.notificationcenterui doNotDisturb -boolean true
 defaults -currentHost write com.apple.notificationcenterui doNotDisturbDate -date "`date -u +\"%Y-%m-%d %H:%M:%S +0000\"`"
-killall NotificationCenter
+sudo killall NotificationCenter
 
 # Disable firewall
 sudo /usr/libexec/ApplicationFirewall/socketfilterfw --setglobalstate off
